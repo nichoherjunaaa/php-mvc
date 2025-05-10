24dfe67306d3bbc5cdb2d50d0e8e5382a6b006c2
nichoherjunaa <nichoherjunaa@gmail.com>
2025-05-10 22:21:14 +0700

    fix:query filter

--- models/Film.php
+++ models/Film.php
@@ -1,8 +1,10 @@
 <?php
 require_once('config/Database.php');
 
-class Film {
-    public static function getAllFilms() {
+class Film
+{
+    public static function getAllFilms()
+    {
         $conn = Database::getConnection();
         $sql = "SELECT * FROM films";
         $result = mysqli_query($conn, $sql);
@@ -16,7 +18,8 @@ class Film {
         return $films;
     }
 
-    public static function searchFilms($keyword) {
+    public static function searchFilms($keyword)
+    {
         $conn = Database::getConnection();
         $keyword = mysqli_real_escape_string($conn, $keyword);
         $sql = "SELECT * FROM films WHERE title LIKE '%$keyword%'";
@@ -31,12 +34,23 @@ class Film {
         return $films;
     }
 
-    public static function filterFilms($genre, $year, $studio) {
+    public static function filterFilms($genre, $year, $studio)
+    {
         $conn = Database::getConnection();
         $conditions = [];
-        if ($genre) $conditions[] = "genre = '" . mysqli_real_escape_string($conn, $genre) . "'";
-        if ($year) $conditions[] = "release_year = '" . mysqli_real_escape_string($conn, $year) . "'";
-        if ($studio) $conditions[] = "studio = '" . mysqli_real_escape_string($conn, $studio) . "'";
+
+        if ($genre) {
+            $genreEscaped = mysqli_real_escape_string($conn, $genre);
+            $conditions[] = "genre LIKE '%$genreEscaped%'";
+        }
+        if ($year) {
+            $yearEscaped = mysqli_real_escape_string($conn, $year);
+            $conditions[] = "release_year = '$yearEscaped'";
+        }
+        if ($studio) {
+            $studioEscaped = mysqli_real_escape_string($conn, $studio);
+            $conditions[] = "studio = '$studioEscaped'";
+        }
 
         $whereClause = count($conditions) ? "WHERE " . implode(" AND ", $conditions) : "";
         $sql = "SELECT * FROM films $whereClause";
